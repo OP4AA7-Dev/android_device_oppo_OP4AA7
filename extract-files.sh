0c6b7bde08e9c586b9a0858d3a592af4e8e0daf6
WenHao2130 <wenhao2130@outlook.com>
2025-11-29 18:40:05 +0800

    OP4AA7: Register proximity fake as android.sensor.proximity

--- extract-files.sh
+++ extract-files.sh
@@ -60,7 +60,9 @@ function blob_fixup() {
         ;;
         vendor/lib64/sensors.ssc.so)
             sed -i "s/qti.sensor.wise_light/android.sensor.light\x00/" "${2}"
+            sed -i "s/qti.sensor.proximity_fake/android.sensor.proximity\x00/" "${2}"
             "${SIGSCAN}" -p "F9 69 CA 84 52 49 3F A0 72" -P "F9 A9 00 80 52 09 00 A0 72" -f "${2}" # mov w9, #0x1fa2653 -> mov w9, #5
+            "${SIGSCAN}" -p "F9 A9 C7 84 52 49 3F A0 72" -P "F9 09 01 80 52 1F 20 03 D5" -f "${2}" # mov w9, #0x1fa263d -> mov w9, #8
         ;;
     esac
 }
