7ce18ecc6e351a9ac658f0441a498c8633bc5a02
Edwin Moquete <edwinmmoquete@gmail.com>
2026-01-12 21:40:14 +0800

    OP4AA7: Switch to legacy sepolicy_vndr

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,7 +172,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 VENDOR_SECURITY_PATCH := 2022-09-05
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 # Treble
 BOARD_VNDK_VERSION := current
