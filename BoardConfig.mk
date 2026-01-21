741fdc999950a1b62fe5724487ac1bc75a36b1f8
Alexander Koskovich <akoskovich@pm.me>
2026-01-21 23:34:07 +0800

    OP4AA7: Remove virtual framebuffer
    * It's largely for testing purposes and it consumes
      a large amount of memory. If your hardware doesn't
      have graphics then it can be useful, which isn't the
      case for shipped devices.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,8 +80,7 @@ BOARD_KERNEL_CMDLINE := \
     msm_rtb.filter=0x237 \
     printk.devkmsg=on \
     service_locator.enable=1 \
-    swiotlb=1 \
-    video=vfb:640x400,bpp=32,memsize=3072000
+    swiotlb=1
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 # TODO: Set SELinux to Permissive mode
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
