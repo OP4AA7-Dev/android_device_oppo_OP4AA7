aa85b6b27d4b08e3152dc0cca8e29ddbdc75c205
Ying-Chen <2132703527@qq.com>
2026-06-21 08:43:41 +0800

    OP4AA7: Add shim for missing symbol in lib-imsvt.so Fixed:     E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: cannot locate symbol "_ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb" referenced by "/system/system_ext/lib64/lib-imsvideocodec.so"...

--- device.mk
+++ device.mk
@@ -403,6 +403,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    lib-imsvtshim \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
